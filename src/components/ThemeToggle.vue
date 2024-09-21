94238f9f7d219646a69a7285fd50eb919a870a7c
zac <zac@zacoons.com>
2024-09-21 15:52:26 +1000

    initial commit

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { setIsLightTheme, getIsLightTheme } from "../utils/theme";
+
+	export default {
+		data() {
+			return {
+				isLightMode: false
+			};
+		},
+		mounted() {
+			this.isLightMode = getIsLightTheme();
+		},
+		methods: {
+			setTheme(e: Event) {
+				const el = e.target as HTMLInputElement;
+				this.isLightMode = el.checked;
+				setIsLightTheme(el.checked);
+			},
+		},
+	};
+</script>
+
+<template>
+	<input
+		type="checkbox"
+		:checked="isLightMode"
+		@change="setTheme"/>
+</template>
