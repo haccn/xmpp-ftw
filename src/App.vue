33f4388c2bade2fca98f14b447658f0207312a17
zac <zac@zacoons.com>
2024-09-23 17:29:23 +1000

    connection and login details are now stored
    added transport to config file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 	import { RouterLink, RouterView } from "vue-router";
-	import HelloWorld from "./components/HelloWorld.vue";
-	import ThemeToggle from "./components/ThemeToggle.vue";
-	import { setIsLightTheme, getIsLightTheme } from "./utils/theme";
+	import ThemeToggle from "@/components/ThemeToggle.vue";
+	import { setIsLightTheme, getIsLightTheme } from "@/utils/theme";
 
 	setIsLightTheme(getIsLightTheme());
 </script>
@@ -12,14 +11,10 @@
 		<div class="wrapper">
 			<nav>
 				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-				<RouterLink to="/login">login</RouterLink>
+				<ThemeToggle />
 			</nav>
 		</div>
 	</header>
-
-	<ThemeToggle />
-	<HelloWorld msg="aaa" />
 
 	<RouterView />
 </template>
@@ -55,30 +50,30 @@ nav a:first-of-type {
 	border: 0;
 }
 
-  @media (min-width: 1024px) {
-	  header {
-		  display: flex;
-		  place-items: center;
-		  padding-right: calc(var(--section-gap) / 2);
-	  }
+@media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
 
-	  .logo {
-		  margin: 0 2rem 0 0;
-	  }
+	.logo {
+		margin: 0 2rem 0 0;
+	}
 
-	  header .wrapper {
-		  display: flex;
-		  place-items: flex-start;
-		  flex-wrap: wrap;
-	  }
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
 
-	  nav {
-		  text-align: left;
-		  margin-left: -1rem;
-		  font-size: 1rem;
+	nav {
+		text-align: left;
+		margin-left: -1rem;
+		font-size: 1rem;
 
-		  padding: 1rem 0;
-		  margin-top: 1rem;
-	  }
-  }
+		padding: 1rem 0;
+		margin-top: 1rem;
+	}
+}
 </style>
